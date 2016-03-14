2f79f8e1df6d796ff6165b90c70b90be26e370a3
shweta1 <shweta.budhiraja@targetintegration.com>
2016-03-14 11:24:12 +0000

    inserted values in tables mobile and features of db2

---
+++ sql/insert_db2_mobile2_mobile.sql
@@ -0,0 +1,24 @@
+insert into mobile(name,model_num) VALUES ("REDMI",1);
+insert into mobile(name,model_num) VALUES ("SAMSUNG",112);
+insert into mobile(name,model_num) VALUES ("HTC",234);
+insert into mobile(name,model_num) VALUES ("Nokia",674);
+insert into mobile(name,model_num) VALUES ("REDMI",46);
+insert into mobile(name,model_num) VALUES ("REDMI",23);
+
+
+
+
+insert into features(feature_name,value) VALUES ("cpu",1);
+insert into features(feature_name,value) VALUES ("camera",0);
+insert into features(feature_name,value) VALUES ("version",1);
+insert into features(feature_name,value) VALUES ("model_num",0);
+insert into features(feature_name,value) VALUES ("version",0);
+insert into features(feature_name,value) VALUES ("cpu",0);
+insert into features(feature_name,value) VALUES ("camera",1);
+
+
+
+
+
+
+
