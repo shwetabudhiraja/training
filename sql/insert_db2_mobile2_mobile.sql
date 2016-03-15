94158700b1a208f04f8be28f111265bb614f6043
shweta1 <shweta.budhiraja@targetintegration.com>
2016-03-15 04:39:34 +0000

    change the column name

--- sql/insert_db2_mobile2_mobile.sql
+++ sql/insert_db2_mobile2_mobile.sql
@@ -1,20 +1,21 @@
-insert into mobile(name,model_num) VALUES ("REDMI",1);
-insert into mobile(name,model_num) VALUES ("SAMSUNG",112);
-insert into mobile(name,model_num) VALUES ("HTC",234);
-insert into mobile(name,model_num) VALUES ("Nokia",674);
-insert into mobile(name,model_num) VALUES ("REDMI",46);
-insert into mobile(name,model_num) VALUES ("REDMI",23);
+insert into mobile2.mobile(name,model_num) VALUES ("REDMI",1);
+insert into mobile2.mobile(name,model_num) VALUES ("SAMSUNG",112);
+insert into mobile2.mobile(name,model_num) VALUES ("HTC",234);
+insert into mobile2.mobile(name,model_num) VALUES ("Nokia",674);
+insert into mobile2.mobile(name,model_num) VALUES ("REDMI",46);
+insert into mobile2.mobile(name,model_num) VALUES ("REDMI",23);
 
 
 
 
-insert into features(feature_name,value) VALUES ("cpu",1);
-insert into features(feature_name,value) VALUES ("camera",0);
-insert into features(feature_name,value) VALUES ("version",1);
-insert into features(feature_name,value) VALUES ("model_num",0);
-insert into features(feature_name,value) VALUES ("version",0);
-insert into features(feature_name,value) VALUES ("cpu",0);
-insert into features(feature_name,value) VALUES ("camera",1);
+insert into mobile2.features(feature_name,value) VALUES ("processor",1);
+insert into mobile2.features(feature_name,value) VALUES ("camera",0);
+insert into mobile2.features(feature_name,value) VALUES ("version",1);
+insert into mobile2.features(feature_name,value) VALUES ("model_num",0);
+insert into mobile2.features(feature_name,value) VALUES ("version",0);
+insert into mobile2.features(feature_name,value) VALUES ("processor",0);
+insert into mobile2.features(feature_name,value) VALUES ("camera",1);
+insert into mobile2.features(feature_name,value) VALUES ("version",1);
 
 
 
